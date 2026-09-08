337b9f10daae60d8b238c95aaaa6bad54ba7757a
Vynix (Phoenix) <kitiwiel@gmail.com>
2026-09-08 09:29:30 +0200

    Add files via upload

--- dist/start.js
+++ dist/start.js
@@ -1,3 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const start_bot_1 = require("./bots/One Bot Nightly/start-bot");
+start_bot_1.bot.on('login', () => {
+    console.log('Bot has logged in');
+});
 //# sourceMappingURL=start.js.map
--- src/start.ts
+++ src/start.ts
@@ -1,2 +1,5 @@
 import { bot } from "./bots/One Bot Nightly/start-bot";
 
+bot.on('login', () => {
+  console.log('Bot has logged in');
+});
